69c283c58376e33a68273271952080c3765ae28f
Arthur Gramiscelli Branco <43115153+reconectar@users.noreply.github.com>
2019-03-26 08:33:23 -0300

    Update lista33.java

--- src/lista33.java
+++ src/lista33.java
@@ -1,15 +1,20 @@
 public class lista33 {
 	public static void main(String[] args) {
 		CPilha pilha1 = new CPilha();
-		CFila fila1 = new CFila();
+		CFila fila2 = new CFila();
 
 		for (int i = 0; i < 10; i++) {
 			pilha1.empilha(i + 1);
 		}
 
 		for (int i = 0; i < 10; i++) {
-			System.out.println(pilha1.peek());
-			fila1.enfileira(pilha1.desempilha());
+			fila2.enfileira(pilha1.desempilha());
 		}
+      
+      for (int i = 0; i < 10; i++) {
+         		 pilha1.empilha(fila2.peek());
+			 System.out.println(fila2.desenfileira());
+		}
+
 	}
 }
